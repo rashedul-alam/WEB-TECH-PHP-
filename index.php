ff0cb99f9f4f92caa46ccd435d7905e921d94597
rashedul-alam <26365486+rashedul-alam@users.noreply.github.com>
2018-06-07 09:40:21 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+	echo "hoooo hoooo<br>";
+	$x = 0;
+	echo $x."<br>";
+	$c =0;
+	echo "<br>";
+	while($c <11)
+	{
+		echo $c++.":RED"."<br>";
+	}
+	echo "------------------------------"."<br>";
+	
+	$n = array("one","two","three" ,"four","five");
+	$b = 0;
+	$d = 0;
+	while($b<=4)
+	{
+		
+		echo $n[$d]." :RED"."<br>";
+		$d++;
+		
+		$b++;
+	}
+	
+	
+	
+	?>
